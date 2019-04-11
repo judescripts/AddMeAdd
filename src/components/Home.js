c90d6026602b1239d10584afea84b4dc4877739f
Jude Lee <judescripts@gmail.com>
2019-04-11 00:48:03 -0700

    updated relative alignments on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,13 +8,17 @@ class Home extends React.Component {
     return (
       <Container className="controls">
         <div className="start-button">
-          <i className="far fa-caret-square-right" />
-          Start your campaign
+          <div className="start-contents">
+            <i className="far fa-caret-square-right" />
+            Start your campaign
+          </div>
         </div>
         <div className="spacer" />
         <div className="manage-button">
-          <i className="fas fa-clipboard" />
-          Manage your campaigns
+          <div className="manage-contents">
+            <i className="fas fa-clipboard" />
+            Manage your campaigns
+          </div>
         </div>
       </Container>
     );
